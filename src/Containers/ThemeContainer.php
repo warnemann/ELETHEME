9e34b84e76f9ca4293e14d3eaca3242d11fc8347
warnemann <warnemann@gmail.com>
2017-08-01 06:11:07 +0200

    Create ThemeContainer.php

---
+++ src/Containers/ThemeContainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ELETHEME\Containers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class ThemeContainer
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('Theme::content.Theme');
+    }
+}
