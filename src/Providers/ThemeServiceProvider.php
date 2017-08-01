c5fefcbaee3de5107e9344d6525380f1b34bc95b
warnemann <warnemann@gmail.com>
2017-08-01 06:11:52 +0200

    Create ThemeServiceProvider.php

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ELETHEME\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+  /**
+   * Register the service provider.
+   */
+  public function register()
+  {
+
+  }
+}
